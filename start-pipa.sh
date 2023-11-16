0b89d7e8198895cc4e44a55440334ac61cc4fc36
Varun Soma <vrnsoma@users.noreply.github.com>
2023-11-17 02:46:36 +0530

    Update and rename roomservice.xml to start-pipa.sh

---
+++ start-pipa.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/DD3Boh/android_device_xiaomi_pipa device/xiaomi/pipa -b lineage-20-7
+git clone https://github.com/DD3Boh/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common -b lineage-21
+git clone https://github.com/luka177/android_vendor_xiaomi_pipa vendor/xiaomi/pipa
+git clone https://github.com/DD3Boh/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+git clone https://github.com/DD3Boh/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250 -b staging/lineage-21-2
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+cd bootable/recovery 
+git remote add source https://github.com/luka177/android_bootable_recovery
+git remote update
+git cherry-pick 502f28c
+cd .. && cd ..
+. build/envsetup.sh
+lunch lineage_pipa-userdebug
+mka bacon -j$(nproc --all)
