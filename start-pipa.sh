b2eb44dc4334c4864a9f84b1a2a7674da4ca713c
luka177 <lukapanio@gmail.com>
2024-01-23 18:07:51 +0100

    start-pipa.sh: Get rid of dir changes

--- start-pipa.sh
+++ start-pipa.sh
@@ -4,11 +4,9 @@ git clone https://github.com/luka177/android_vendor_xiaomi_pipa vendor/xiaomi/pi
 git clone https://github.com/DD3Boh/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 git clone https://github.com/DD3Boh/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250 -b staging/lineage-21-2
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
-cd bootable/recovery 
-git remote add source https://github.com/luka177/android_bootable_recovery
-git remote update
-git cherry-pick 502f28c
-cd .. && cd ..
+git -C bootable/recovery remote add source https://github.com/luka177/android_bootable_recovery
+git -C bootable/recovery remote update
+git -C bootable/recovery cherry-pick 502f28c
 . build/envsetup.sh
 lunch lineage_pipa-userdebug
 mka bacon -j$(nproc --all)
